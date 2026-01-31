902043627a7cc30efe4d767598ebf9cac0e67777
Ari Tedeschi <arijun2010@hotmail.com>
2026-01-30 21:53:23 -0300

    chore:
    fixed linter

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 function Home() {
-    return <section>
-        <h1><center>BUILDING</center></h1>
-        <h5><center>by ARI_NAO_MEXE_XD </center></h5>
+  return (
+    <section>
+      <h1>
+        <center>BUILDING</center>
+      </h1>
+      <h5>
+        <center>by ARI_NAO_MEXE_XD </center>
+      </h5>
     </section>
+  );
 }
 
 export default Home;
